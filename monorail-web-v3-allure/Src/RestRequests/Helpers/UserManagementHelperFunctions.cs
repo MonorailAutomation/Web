2a5fdd26e8dccd8c68e6bbb95571a0ccb865c1ae
hannakuczara <hanna.kuczara@gmail.com>
2022-05-04 06:43:08 -0400

    added explanation for difference in using CloseUser and SuspendUser

--- monorail-web-v3-allure/Src/RestRequests/Helpers/UserManagementHelperFunctions.cs
+++ monorail-web-v3-allure/Src/RestRequests/Helpers/UserManagementHelperFunctions.cs
@@ -7,6 +7,17 @@ namespace monorail_web_v3.RestRequests.Helpers
 {
     public static class UserManagementHelperFunctions
     {
+        /*
+         CloseUser(string email) 
+         Sets 'isDeleted' flag as true. We don't have any mechanism to unlock such user again. 
+         Daily jobs will NOT be run for such user. 
+         We will use this function for all users EXCEPT the Orbis ones.
+         
+         SuspendUser(string email) 
+         Sets 'isSuspended' flag as true. We are able to unlock such user.
+         Daily jobs will be run for such user.
+         We will use this function ONLY for the Orbis users.
+         */
         public static void CloseUser(string email)
         {
             var userId = GetUserId(email);
